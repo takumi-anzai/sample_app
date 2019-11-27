6c0c58a1da2fab5a78995dcd1f8e2c57fd6262c4
takumi-anzai <takumi.anzai@seattleconsulting.co.jp>
2019-11-27 23:36:17 +0900

    fix:hemo

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  def hemo
+  def home
   end
 
   def help
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get hemo" do
-    get static_pages_hemo_url
+  test "should get home" do
+    get static_pages_home_url
     assert_response :success
   end
 
